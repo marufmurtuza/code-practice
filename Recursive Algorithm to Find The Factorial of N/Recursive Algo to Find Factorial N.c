b923803277c6152ea8c4884a72b50b5257d62df2
Maruf Bin Murtuza <marufbinmurtuza@protonmail.ch>
2022-03-06 09:25:23 +0600

    Add files via upload

---
+++ Recursive Algorithm to Find The Factorial of N/Recursive Algo to Find Factorial N.c
@@ -0,0 +1,34 @@
+// Implementation of a recursive algorithm to find factorial of n.   
+
+
+#include <stdio.h>
+
+int factorial(int n){
+
+    if(n==1 || n==0){
+
+      return 1;
+
+    }else{
+
+      int x = (n*factorial(n-1));
+
+      return x;
+
+    }
+
+}
+
+int main(){
+
+    int n;
+
+    printf("Enter Number: ");
+
+    scanf("%d",&n);
+
+    printf("\nGiven Number: %d\n",n);
+
+    printf("Factorial: %d\n",factorial(n));
+
+}
