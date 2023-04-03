fe99a9d13a5ea23b94b8ac452c266c971fa4780d
Maruf Bin Murtuza <78444913+marufmurtuza@users.noreply.github.com>
2023-04-03 15:42:18 +0600

    Add files via upload

---
+++ Codecademy/JS/dog_years.js
@@ -0,0 +1,14 @@
+let myAge = 10;
+var earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears = earlyYears + laterYears;
+var myName = "Mr.Panda".toLowerCase();
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is 
+${myAgeInDogYears} years old in dog years.`
+);
+
