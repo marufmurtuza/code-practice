97caaeb312d7e6ce1686c57a183c2fc705efe1bf
Maruf Bin Murtuza <78444913+marufmurtuza@users.noreply.github.com>
2023-04-03 15:40:47 +0600

    Add files via upload

---
+++ Codecademy/JS/kelvin_weather.js
@@ -0,0 +1,7 @@
+const kelvin = 0;
+var celsius = kelvin - 273;
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+var newton = Math.floor(celsius * (33 / 100));
+console.log(`The temperature in Newton is ${newton}`);
+
